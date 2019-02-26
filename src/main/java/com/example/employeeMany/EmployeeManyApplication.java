4099c2b22ec5675c3922e9608df77551ca717794
Thomas Kendrick <tommkendrick@gmail.com>
2019-02-26 10:57:58 +0000

    initial commit

---
+++ src/main/java/com/example/employeeMany/EmployeeManyApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeeMany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManyApplication.class, args);
+	}
+
+}
